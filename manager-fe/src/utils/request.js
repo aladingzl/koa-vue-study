feaebaae3240bd174d27fec1b3cf4c43eacfe66d
aladingzl <2414151633@qq.com>
2022-04-09 23:41:10 +0800

    feat: storage

--- manager-fe/src/utils/request.js
+++ manager-fe/src/utils/request.js
@@ -2,10 +2,11 @@
  *  axios封装
  */
 
-import axios from 'axios'
-import config from './../config'
-import { ElMessage } from 'element-plus'
-import router from './../router'
+import axios from 'axios';
+import config from '../config';
+import { ElMessage } from 'element-plus';
+import router from '../router';
+import storage from './storage';
 
 const TOKEN_INVALID = 'Token认证失败，请重新登录'
 const NETWORK_ERROR = '网络请求异常，请稍后重试'
@@ -19,7 +20,7 @@ const service = axios.create({
 // 请求拦截
 service.interceptors.request.use((req) => {
   const headers = req.headers;
-  const { token } = 'token';
+  const { token } = storage.getItem('');
   if(!headers.Authorization) {
     headers.Authorization = 'Bearer' + token;
   }
